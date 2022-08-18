f61102a2a81d57d2efd89984640c21db89a2beae
Lex Pan <lexiang.pan@gmail.com>
2022-08-18 18:28:28 -0400

    implemented google books api to search for books

--- website_functions.js
+++ website_functions.js
@@ -28,3 +28,55 @@ document.addEventListener('click', e => {
         }
     }
 })
+
+
+function mbnSearch() {
+    if (event.key === "Enter") {
+        let userInput = document.getElementById("mbnSearchBar").value;
+
+        fetch("https://www.googleapis.com/books/v1/volumes?q=" + userInput + "&maxResults=39")
+            .then(res => res.json())            
+            .then(data => {
+                displaySearchResults(data);
+            })
+            
+    }
+}
+
+function displaySearchResults(data) {
+    let checkSearch = document.getElementById('all-movies-display')
+
+    let divsWithin = checkSearch.getElementsByTagName('div')
+    while (divsWithin.length > 0) {
+        divsWithin[0].remove()
+    }
+
+    for (i=0; i < data.items.length; i++){
+        const dataInfo = data.items[i].volumeInfo
+
+        const dataDiv = document.createElement('div');
+        dataDiv.classList.add("media-display")
+
+
+        const dataTitle = document.createElement('h3');
+        dataTitle.classList.add("media-title")
+        dataTitle.innerHTML = dataInfo.title;
+
+        const dataImage = document.createElement('img');
+        dataImage.classList.add("book-or-movie-image")
+        dataImage.src = dataInfo.imageLinks.thumbnail;
+
+        const dataDescription = document.createElement('p');
+        dataDescription.classList.add("media-description")
+        dataDescription.innerHTML = dataInfo.description;
+
+
+        dataDiv.appendChild(dataTitle);
+        dataDiv.appendChild(dataImage);
+        dataDiv.appendChild(dataDescription);
+
+        const displaySection = document.getElementById('all-movies-display');
+        displaySection.appendChild(dataDiv);
+
+    }
+}
